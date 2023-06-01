1307ba4e58b364725c249414fa40c32b1cb665ee
Allan <AGawron2021@protonmail.com>
2023-06-01 20:31:31 +0200

    Numerous Changes

--- BasicNameGeneratorCS/Program.cs
+++ BasicNameGeneratorCS/Program.cs
@@ -1,56 +1,77 @@
 ﻿using System.Text;
+using System.IO;
 
-while (true)
+namespace BasicNameGeneratorCS
 {
-    //These need to be in the debug folder/net7.0, during development, or the folder containing the executable on release
-    string maleOutput = @"Output\\OutputMale.txt", femaleOutput = @"Output\\OutputFemale.txt";
-    int UserInput, MaxGen, Counter = 0;
-    Console.Clear();
-
-    Console.SetCursorPosition(0, 1);
-    Console.Write("     Type '1' to generate male names\n" +
-                  "     Type '2' to generate female names\n");
-    Console.SetCursorPosition(5, 27);
-    Console.Write("Choose: ");
-    bool UserInputParse = Int32.TryParse(Console.ReadLine(), out UserInput);
-    Console.Clear();
-
-    switch (UserInput)
+    internal class Program
     {
-        case 1:
-            Console.SetCursorPosition(5, 1);                                            //put this into repeateable method
-            Console.Write("Type the number of names to generate: ");
-            bool MaxGenParse1 = Int32.TryParse(Console.ReadLine(), out MaxGen);
-            Console.Clear();
-            while (Counter != MaxGen)
+        static void Main()
+        {
+            while (true)
             {
-                string randomName = BasicNameGeneratorCS.NameGenerator.GenerateNameMale();
-                var WriteToFile = new StreamWriter(maleOutput, true, Encoding.ASCII);
-                WriteToFile.WriteLine(randomName);
-                WriteToFile.Close();
-                Console.WriteLine("     " + randomName);
-                Counter++;
+                string maleOutput = @"Output\\OutputMale.txt", femaleOutput = @"Output\\OutputFemale.txt";
+                string[] namesMale = File.ReadAllLines(@"Data\\NamesMale.txt");
+                string[] namesFemale = File.ReadAllLines(@"Data\\NamesFemale.txt");
+                string[] namesLast = File.ReadAllLines(@"Data\\LastNames.txt");
+                if (!File.Exists(maleOutput)) { File.Create(maleOutput).Close();}
+                if (!File.Exists(femaleOutput)) { File.Create(femaleOutput).Close();}
+
+                int UserInput, MaxGen, Counter = 0;
+                Console.Clear();
+                Console.SetCursorPosition(0, 1);
+                Console.Write("     Type '1' to generate male names\n" +
+                              "     Type '2' to generate female names\n");
+                Console.SetCursorPosition(5, 27);
+                Console.Write("Choose: ");
+                bool UserInputParse = Int32.TryParse(Console.ReadLine(), out UserInput);
+                Console.Clear();
+
+                switch (UserInput)
+                {
+                    case 1:
+                        Console.SetCursorPosition(5, 1);
+                        Console.Write("Type the number of names to generate: ");
+                        bool MaxGenParse1 = Int32.TryParse(Console.ReadLine(), out MaxGen);
+                        Console.Clear();
+                        while (Counter != MaxGen)
+                        {
+                            string randomName = GenerateName(namesMale, namesLast);
+                            var WriteToFile = new StreamWriter(maleOutput, true, Encoding.ASCII);
+                            WriteToFile.WriteLine(randomName);
+                            WriteToFile.Close();
+                            Console.WriteLine("     " + randomName);
+                            Counter++;
+                        }
+                        break;
+                    case 2:
+                        Console.SetCursorPosition(5, 1);
+                        Console.Write("Type the number of names to generate: ");
+                        bool MaxGenParse2 = Int32.TryParse(Console.ReadLine(), out MaxGen);
+                        Console.Clear();
+                        while (Counter != MaxGen)
+                        {
+                            string randomName = GenerateName(namesFemale, namesLast);
+                            var WriteToFile = new StreamWriter(femaleOutput, true, Encoding.ASCII);
+                            WriteToFile.WriteLine(randomName);
+                            WriteToFile.Close();
+                            Console.WriteLine("     " + randomName);
+                            Counter++;
+                        }
+                        break;
+                    default:
+                        Console.SetCursorPosition(5, 1);
+                        Console.Write("Invalid input, press enter to try again:");
+                        break;
+                }
+                Console.ReadKey();
             }
-            break;
-        case 2:
-            Console.SetCursorPosition(5, 1);
-            Console.Write("Type the number of names to generate: ");
-            bool MaxGenParse2 = Int32.TryParse(Console.ReadLine(), out MaxGen);
-            Console.Clear();
-            while (Counter != MaxGen)
+            static string GenerateName(string[] firstNames, string[]lastNames)
             {
-                string randomName = BasicNameGeneratorCS.NameGenerator.GenerateNameFemale();
-                var WriteToFile = new StreamWriter(femaleOutput, true, Encoding.ASCII);
-                WriteToFile.WriteLine(randomName);
-                WriteToFile.Close();
-                Console.WriteLine("     " + randomName);
-                Counter++;
+                Random rand = new Random();
+                string randomFirstName = firstNames[rand.Next(firstNames.Length)];
+                string randomLastName = lastNames[rand.Next(lastNames.Length)];
+                return $"{randomFirstName} {randomLastName}";
             }
-            break;
-        default:
-            Console.SetCursorPosition(5, 1);
-            Console.Write("Invalid input, press enter to try again:");
-            break;
+        }
     }
-    Console.ReadKey();
 }
